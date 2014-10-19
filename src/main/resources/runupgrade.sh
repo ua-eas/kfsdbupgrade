beb708f17d4db3be033c985e4b692275c08c4e19
rbtucker <rbtucker@email.arizona.edu>
2014-10-19 07:02:44 -0700

    making changes for upgrade to kfs 3

--- src/main/resources/runupgrade.sh
+++ src/main/resources/runupgrade.sh
@@ -1,2 +1,2 @@
-clear
-java -Xmx500m -jar kfsdbupgrade.jar /home/rbtucker/tmp/kfsdbupgrade.properties
+rm nohup.out
+nohup java -Xmx1g -jar kfsdbupgrade-jar-with-dependencies.jar [upgrade-work-directory]/kfsdbupgrade.properties &
