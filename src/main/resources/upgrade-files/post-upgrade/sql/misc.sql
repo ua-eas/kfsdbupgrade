38f6cce1658b61646b016c6f08fe895c33b73237
rbtucker <rbtucker@email.arizona.edu>
2014-11-24 07:43:45 -0700

    fixed problem workflow files

--- src/main/resources/upgrade-files/post-upgrade/sql/misc.sql
+++ src/main/resources/upgrade-files/post-upgrade/sql/misc.sql
@@ -1,2 +1,3 @@
 alter table KRLC_CNTRY_T add ALT_POSTAL_CNTRY_CD VARCHAR2(3);
 update krim_rsp_t set ver_nbr = 1 where ver_nbr = 0;
+insert into krim_grp_t (grp_id, obj_id, ver_nbr, grp_nm, nmspc_cd, kim_typ_id, actv_ind, last_updt_dt) values (krim_grp_id_s.nextVal, sys_guid(), 1, 'WorkflowAdmin', 'KR-WKFLW', 'KFS68', 'Y', sysdate);
