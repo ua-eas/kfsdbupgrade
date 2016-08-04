91628dd56e30cadcf7ddc6bfc08316bbd47de985
William Austin <quikkian@email.arizona.edu>
2016-08-04 14:53:15 -0700

    UAF-2377 - adding header comment to .sql file

--- src/main/resources/upgrade-files/post-upgrade/sql/UAF-2377-PopulateW8Type.sql
+++ src/main/resources/upgrade-files/post-upgrade/sql/UAF-2377-PopulateW8Type.sql
@@ -1,3 +1,6 @@
+-- =================================================================================================
+--  Insert W-8 type maintenance table values during database upgrade (UAF-2377) subtask of (UAF-92)
+-- =================================================================================================
 INSERT INTO PUR_VNDR_W8_TYP_T (VNDR_W8_TYP_CD, OBJ_ID, VER_NBR, VNDR_W8_TYP_DESC, ACTV_IND) VALUES ('W1', SYS_GUID(), 1, 'W-8BEN', 'Y');
 INSERT INTO PUR_VNDR_W8_OWNRSHP_T (ID, OBJ_ID, VER_NBR, VNDR_W8_TYP_CD, VNDR_OWNRSHIP_CD, ACTV_IND) VALUES ('W1', SYS_GUID(), 1, 'W1', 'FV', 'Y');
 INSERT INTO PUR_VNDR_W8_TYP_T (VNDR_W8_TYP_CD, OBJ_ID, VER_NBR, VNDR_W8_TYP_DESC, ACTV_IND) VALUES ('W2', SYS_GUID(), 1, 'W-8BEN-E', 'Y');
