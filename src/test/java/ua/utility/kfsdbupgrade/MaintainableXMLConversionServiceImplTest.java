f9ed29fb14b16740d272e82c54fadceb4dac234a
William Austin <quikkian@email.arizona.edu>
2016-08-13 14:16:08 -0700

    UAF-2980 - adding @Ignore annotation to test that is meant to be run locally, not on continuous integration server

--- src/test/java/ua/utility/kfsdbupgrade/MaintainableXMLConversionServiceImplTest.java
+++ src/test/java/ua/utility/kfsdbupgrade/MaintainableXMLConversionServiceImplTest.java
@@ -4,11 +4,14 @@ import java.io.File;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public final class MaintainableXMLConversionServiceImplTest {
 
 	@Test
+	@Ignore
+	// test to manually run a conversion on a developer's workstation
 	public void convert() throws Exception {
 		File f = new File("src/main/resources/MaintainableXMLUpgradeRules.xml");
 		MaintainableXMLConversionServiceImpl maintainableXMLConversionServiceImpl = new MaintainableXMLConversionServiceImpl(
