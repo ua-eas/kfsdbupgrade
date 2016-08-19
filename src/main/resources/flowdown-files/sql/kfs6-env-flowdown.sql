c82b7017ccd006889c792f0750d15d9fc814cc15
William Austin <quikkian@email.arizona.edu>
2016-08-19 14:42:02 -0700

    UAF-3052 - Adding 'nataliac' to list of backdoor users.

--- src/main/resources/flowdown-files/sql/kfs6-env-flowdown.sql
+++ src/main/resources/flowdown-files/sql/kfs6-env-flowdown.sql
@@ -956,5 +956,5 @@ values (KRIM_ROLE_PERM_ID_S.NEXTVAL, 1, sys_guid(),
 insert into krim_role_mbr_t ( role_mbr_id, ver_nbr, obj_id, role_id, mbr_id, mbr_typ_cd) 
   select KRIM_ROLE_MBR_ID_S.NEXTVAL, 1, SYS_GUID(), (select role_id from krim_role_t where role_nm='Back Door Login'), prncpl_id, 'P' 
   from kulowner.krim_entity_cache_t 
-  where prncpl_nm in ('kbasu', 'gurtonj', 'akhilashokk', 'elvirag', 'jpfennig', 'kosta', 'jwingate', 'mccunej', 'maryb', 'sandovar', 'kbrook', 'robbiem', 'fischerm', 'mmoen', 'ketchmark', 'lilas', 'jnschool', 'melissav73', 'sskinner', 'rdubisar', 'marcel', 'janifisk', 'sandberm', 'mhohl', 'ake27', 'hlo', 'kristinalopez1', 'leahasullivan', 'alee', 'perryg', 'maramian', 'rblank', 'tammyv', 'buchanaw', 'jreese','quikkian','tounou','amyrodriguez','wames')
+  where prncpl_nm in ('kbasu', 'gurtonj', 'akhilashokk', 'elvirag', 'jpfennig', 'kosta', 'jwingate', 'mccunej', 'maryb', 'sandovar', 'kbrook', 'robbiem', 'fischerm', 'mmoen', 'ketchmark', 'lilas', 'jnschool', 'melissav73', 'sskinner', 'rdubisar', 'marcel', 'janifisk', 'sandberm', 'mhohl', 'ake27', 'hlo', 'kristinalopez1', 'leahasullivan', 'alee', 'perryg', 'maramian', 'rblank', 'tammyv', 'buchanaw', 'jreese','quikkian','tounou','amyrodriguez','wames','nataliac')
 ;
