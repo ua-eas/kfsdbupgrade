2d739b70189a9d4f734d89212b56a08250168b4a
Jim Reese <jreese@email.arizona.edu>
2017-03-30 09:15:45 -0700

    UAF-4177 : Update parameters for Cash Receipt. Per HLo this should be applied instead as a liquibase changelog on Tomcat start. Hence removing from KFSDBUpgrade post-upgrade scripts and kfsdbupgrade.properties file.

--- src/main/resources/upgrade-files/post-upgrade/sql/UAF-4177_Update_parameters_for_Cash_Receipt.sql
+++
@@ -1,4 +0,0 @@
--- =============================================================================
--- UAF-4177 : Update parameters for Cash Receipt. Subtask of (UAF-92)
--- =============================================================================
-UPDATE KULOWNER.KRCR_PARM_T SET VAL = 'N' WHERE PARM_NM='DISPLAY_CASH_RECEIPT_DENOMINATION_DETAIL_IND';
