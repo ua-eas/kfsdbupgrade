b02ec15b0e49f0b14bdd446897de935ae6de3e8c
S.G. Skinner <sskinner@email.arizona.edu>
2017-05-04 18:08:57 -0700

    UAF-3849 - Last query passing tests now.

--- src/test/java/ua/utility/kfsdbupgrade/GecBootstrapDataTest.java
+++ src/test/java/ua/utility/kfsdbupgrade/GecBootstrapDataTest.java
@@ -11,19 +11,35 @@ import org.junit.Test;
  * Created by S.G. Skinner on 2017-05-04
  */
 public class GecBootstrapDataTest extends AppTestBase {
-    private static final String PROBLEM_QUERY_FILE_1 = "gec_test_queries/uaf-3849_problem_queries.sql";
+    private static final String PROBLEM_QUERY_FILE_1 = "gec_test_queries/uaf-3849_problem_query_1.sql";
+    private static final String PROBLEM_QUERY_FILE_2 = "gec_test_queries/uaf-3849_problem_query_2.sql";
 
     /*
-     * Test query that failed for syntax reasons, isolated for testing. If you see this
+     * Test query that failed with a double-quoted string, now isolated for testing. If you see this
      * in the master branch, this test should pass.
      *
-     * Test prep: Fully Upgraded DB, with truncated FP_GEC_ENTRY_REL_T; this will have the no-op
+     * Test prep: Fully Upgraded DB, with truncated FP_GEC_ENTRY_REL_T; this will have a no-op
      *            effect, since the inner query of the file counts against a now empty table. But,
      *            we will still get validation of it being sent off to the server
      */
     @Test
-    public void testProblemQuerySubset () {
+    public void testProblemQuery_1 () {
         boolean success = executeSqlFile(PROBLEM_QUERY_FILE_1);
+        Assert.assertTrue("Query file did NOT process successfully!", success);
+    }
+
+
+    /*
+     * Test query that hasn't been proven yet; we want to make sure it doesn't cost a 12h run
+     * and then fail at the very end (this is 12/12 from the main GEC script).
+     *
+     * Test prep: Fully Upgraded DB, with truncated FP_GEC_ENTRY_REL_T; this will have a no-op
+     *            effect, since the inner query of the file counts against a now empty table. But,
+     *            we will still get validation of it being sent off to the server
+     */
+    @Test
+    public void testProblemQuery_2 () {
+        boolean success = executeSqlFile(PROBLEM_QUERY_FILE_2);
         Assert.assertTrue("Query file did NOT process successfully!", success);
     }
 
