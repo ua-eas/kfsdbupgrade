532d6026bd2e8d8c852932a313e55d6036517371
Jim Reese <jreese@email.arizona.edu>
2017-10-05 16:36:43 -0700

    UAF-5966 : Modify Backdoor Access for UAF 7 Environments for cecewilson, robertafrench & deniseblum. Added SQL INSERTs to file 'flowdown-files/sql/kfs6-env-flowdown.sql' to enable Backdoor access for: Celia Wilson (cecewilson), Roberta French (robertafrench) and Denise Blum (deniseblum).

--- src/main/resources/flowdown-files/sql/kfs6-env-flowdown.sql
+++ src/main/resources/flowdown-files/sql/kfs6-env-flowdown.sql
@@ -1037,3 +1037,9 @@ insert into krim_role_mbr_t ( role_mbr_id, ver_nbr, obj_id, role_id, mbr_id, mbr
 insert into krim_role_mbr_t ( role_mbr_id, ver_nbr, obj_id, role_id, mbr_id, mbr_typ_cd) values (KRIM_ROLE_MBR_ID_S.NEXTVAL, 1, SYS_GUID(), (select role_id from krim_role_t where role_nm='Back Door Login'), '119584198629', 'P');
 -- vmolt
 insert into krim_role_mbr_t ( role_mbr_id, ver_nbr, obj_id, role_id, mbr_id, mbr_typ_cd) values (KRIM_ROLE_MBR_ID_S.NEXTVAL, 1, SYS_GUID(), (select role_id from krim_role_t where role_nm='Back Door Login'), '112156828140', 'P');
+-- cecewilson
+insert into krim_role_mbr_t ( role_mbr_id, ver_nbr, obj_id, role_id, mbr_id, mbr_typ_cd) values (KRIM_ROLE_MBR_ID_S.NEXTVAL, 1, SYS_GUID(), (select role_id from krim_role_t where role_nm='Back Door Login'), '103976941930', 'P');
+-- robertafrench
+insert into krim_role_mbr_t ( role_mbr_id, ver_nbr, obj_id, role_id, mbr_id, mbr_typ_cd) values (KRIM_ROLE_MBR_ID_S.NEXTVAL, 1, SYS_GUID(), (select role_id from krim_role_t where role_nm='Back Door Login'), '116183163807', 'P');
+-- deniseblum
+insert into krim_role_mbr_t ( role_mbr_id, ver_nbr, obj_id, role_id, mbr_id, mbr_typ_cd) values (KRIM_ROLE_MBR_ID_S.NEXTVAL, 1, SYS_GUID(), (select role_id from krim_role_t where role_nm='Back Door Login'), '105159912253', 'P');
