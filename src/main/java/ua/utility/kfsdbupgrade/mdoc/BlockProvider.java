6b98d4521d62b85888257340260a6ef028fcb9c4
jcaddel <jcaddel@email.arizona.edu>
2017-10-06 18:07:53 -0700

    perf

--- src/main/java/ua/utility/kfsdbupgrade/mdoc/BlockProvider.java
+++ src/main/java/ua/utility/kfsdbupgrade/mdoc/BlockProvider.java
@@ -1,11 +1,9 @@
 package ua.utility.kfsdbupgrade.mdoc;
 
-import static com.google.common.base.Optional.absent;
-import static com.google.common.base.Optional.of;
 import static com.google.common.base.Stopwatch.createStarted;
 import static com.google.common.collect.ImmutableMap.copyOf;
-import static java.lang.Integer.parseInt;
 import static java.lang.Math.round;
+import static java.lang.String.format;
 import static org.apache.log4j.Logger.getLogger;
 import static ua.utility.kfsdbupgrade.log.Logging.info;
 import static ua.utility.kfsdbupgrade.mdoc.Closeables.closeQuietly;
@@ -17,7 +15,6 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Properties;
 
 import javax.inject.Provider;
 
@@ -33,47 +30,50 @@ public final class BlockProvider implements Provider<ImmutableMap<BlockId, RowId
 
   private static final Logger LOGGER = getLogger(BlockProvider.class);
 
-  public BlockProvider(Properties props) {
-    this.props = props;
+  public BlockProvider(Connection conn, String schema, String table, Optional<Integer> max, Optional<Integer> show) {
+    this.conn = conn;
+    this.schema = schema.toUpperCase();
+    this.table = table.toUpperCase();
+    this.max = max;
+    this.show = show;
   }
 
-  private final Properties props;
+  private final Connection conn;
+  private final String schema;
+  private final String table;
+  private final Optional<Integer> max;
+  private final Optional<Integer> show;
+  private final RowIdConverter converter = new RowIdConverter();
 
   public ImmutableMap<BlockId, RowId> get() {
-    Connection conn = null;
     Statement stmt = null;
     ResultSet rs = null;
     try {
-      RowIdConverter converter = new RowIdConverter();
-      int count = 0;
-      Optional<Integer> max = absent();
-      if (props.containsKey("mdoc.max")) {
-        int value = parseInt(props.getProperty("mdoc.max"));
-        max = of(value);
-      }
-      conn = new ConnectionProvider(props, false).get();
-      info(LOGGER, "acquiring -> %s rowids", getCount(max.or(-1)));
+      info(LOGGER, "acquiring -> %s rowids in %s.%s", max.isPresent() ? getCount(max.get()) : "all", schema, table);
       Stopwatch sw = createStarted();
       stmt = conn.createStatement();
-      String where = max.isPresent() ? " where rownum <= " + max.get() : "";
-      rs = stmt.executeQuery("select rowid from krns_maint_doc_t" + where);
+      String where = max.isPresent() ? "WHERE ROWNUM <= " + max.get() : "";
+      rs = stmt.executeQuery(format("SELECT ROWID FROM %s.%s %s", schema, table, where).trim());
+      int count = 0;
       ListMultimap<BlockId, RowId> mm = ArrayListMultimap.create();
       while (rs.next()) {
         String string = rs.getString(1);
         RowId rowId = converter.convert(string);
         mm.put(rowId.getBlock(), rowId);
-        if (++count % 100000 == 0) {
-          info(LOGGER, "%s of %s [%s]", getCount(count), getCount(max.or(-1)), getTime(sw));
+        count++;
+        if (show.isPresent() && count % show.get() == 0) {
+          info(LOGGER, "%s of %s [%s]", getCount(count), max.isPresent() ? getCount(max.get()) : "?", getTime(sw));
         }
       }
-      int size = mm.size();
-      int keys = mm.keySet().size();
-      double percent = (keys * 1d) / size;
+      int rows = mm.size();
+      int blocks = mm.keySet().size();
+      double percent = (blocks * 1d) / rows;
       double reduction = (1 - percent) * 100;
-      info(LOGGER, "elapsed ---> %s", getTime(sw));
-      info(LOGGER, "rows ------> %s", getCount(mm.size()));
-      info(LOGGER, "locations -> %s", getCount(mm.keySet().size()));
-      info(LOGGER, "reduction -> %s%%", round(reduction));
+      info(LOGGER, "elapsed ----> %s", getTime(sw));
+      info(LOGGER, "rows -------> %s", getCount(rows));
+      info(LOGGER, "blocks -----> %s", getCount(blocks));
+      info(LOGGER, "reduction --> %s%%", round(reduction));
+      info(LOGGER, "blocks/row -> %s%%", getCount(blocks / rows));
       Map<BlockId, RowId> map = new LinkedHashMap<>();
       for (BlockId block : mm.keySet()) {
         map.put(block, mm.get(block).iterator().next());
@@ -84,7 +84,6 @@ public final class BlockProvider implements Provider<ImmutableMap<BlockId, RowId
     } finally {
       closeQuietly(rs);
       closeQuietly(stmt);
-      closeQuietly(conn);
     }
 
   }
