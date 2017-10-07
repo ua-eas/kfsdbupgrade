ef9e05670ca1932ff10e877aba31e08b2cf0cf41
jcaddel <jcaddel@email.arizona.edu>
2017-10-07 00:48:53 -0700

    perf

--- src/test/java/ua/utility/kfsdbupgrade/FirstTouchPenaltyTest.java
+++ src/test/java/ua/utility/kfsdbupgrade/FirstTouchPenaltyTest.java
@@ -1,15 +1,22 @@
 package ua.utility.kfsdbupgrade;
 
 import static com.google.common.collect.ImmutableMap.copyOf;
+import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Maps.newLinkedHashMap;
 import static ua.utility.kfsdbupgrade.log.Logging.info;
+import static ua.utility.kfsdbupgrade.mdoc.Callables.fromProvider;
+import static ua.utility.kfsdbupgrade.mdoc.Callables.getFutures;
 import static ua.utility.kfsdbupgrade.mdoc.Formats.getCount;
+import static ua.utility.kfsdbupgrade.mdoc.Lists.concat;
+import static ua.utility.kfsdbupgrade.mdoc.Lists.distribute;
 import static ua.utility.kfsdbupgrade.mdoc.Lists.transform;
 import static ua.utility.kfsdbupgrade.mdoc.MaintDocField.VER_NBR;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
 
 import org.apache.log4j.Logger;
 import org.junit.Test;
@@ -19,6 +26,7 @@ import com.google.common.collect.ImmutableMap;
 
 import ua.utility.kfsdbupgrade.mdoc.BlockId;
 import ua.utility.kfsdbupgrade.mdoc.ConnectionProvider;
+import ua.utility.kfsdbupgrade.mdoc.ExecutorProvider;
 import ua.utility.kfsdbupgrade.mdoc.IntegerWeigher;
 import ua.utility.kfsdbupgrade.mdoc.PropertiesProvider;
 import ua.utility.kfsdbupgrade.mdoc.RowId;
@@ -27,6 +35,7 @@ import ua.utility.kfsdbupgrade.mdoc.RowSelector;
 import ua.utility.kfsdbupgrade.mdoc.SingleIntegerFunction;
 import ua.utility.kfsdbupgrade.mdoc.SingleStringFunction;
 import ua.utility.kfsdbupgrade.mdoc.StringWeigher;
+import ua.utility.kfsdbupgrade.mdoc.ThreadsProvider;
 
 public class FirstTouchPenaltyTest {
 
@@ -53,16 +62,22 @@ public class FirstTouchPenaltyTest {
   private ImmutableList<Integer> touch(Properties props, String table, String field, Iterable<RowId> iterable) {
     List<String> rowIds = transform(iterable, converter.reverse());
     ConnectionProvider provider = new ConnectionProvider(props, false);
-    RowSelector.Builder<Integer> builder = RowSelector.builder();
-    builder.withFunction(SingleIntegerFunction.INSTANCE);
-    builder.withWeigher(IntegerWeigher.INSTANCE);
-    builder.withRowIds(rowIds);
-    builder.withShow(rowIds.size() / 10);
-    builder.withTable(table);
-    builder.withProvider(provider);
-    builder.withField(field);
-    RowSelector<Integer> selector = builder.build();
-    return selector.get();
+    int threads = new ThreadsProvider(props).get();
+    ExecutorService executor = new ExecutorProvider("touch", threads).get();
+    List<Callable<ImmutableList<Integer>>> callables = newArrayList();
+    for (List<String> distribution : distribute(rowIds, threads)) {
+      RowSelector.Builder<Integer> builder = RowSelector.builder();
+      builder.withFunction(SingleIntegerFunction.INSTANCE);
+      builder.withWeigher(IntegerWeigher.INSTANCE);
+      builder.withRowIds(distribution);
+      builder.withShow(rowIds.size() / 10);
+      builder.withTable(table);
+      builder.withProvider(provider);
+      builder.withField(field);
+      RowSelector<Integer> selector = builder.build();
+      callables.add(fromProvider(selector));
+    }
+    return concat(getFutures(executor, callables));
   }
 
   private ImmutableMap<BlockId, RowId> getBlocks(Iterable<RowId> rowIds) {
