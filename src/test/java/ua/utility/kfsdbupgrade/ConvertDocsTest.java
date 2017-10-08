38af742fdfea44d2da65fc3dc46db281a738f5bb
jcaddel <jcaddel@email.arizona.edu>
2017-10-08 01:27:51 -0700

    perf

--- src/test/java/ua/utility/kfsdbupgrade/ConvertDocsTest.java
+++ src/test/java/ua/utility/kfsdbupgrade/ConvertDocsTest.java
@@ -5,6 +5,7 @@ import static com.google.common.base.Stopwatch.createUnstarted;
 import static com.google.common.collect.Lists.newArrayList;
 import static ua.utility.kfsdbupgrade.mdoc.Callables.getFutures;
 import static ua.utility.kfsdbupgrade.mdoc.Lists.distribute;
+import static ua.utility.kfsdbupgrade.mdoc.Lists.shuffle;
 import static ua.utility.kfsdbupgrade.mdoc.Lists.transform;
 import static ua.utility.kfsdbupgrade.mdoc.Show.show;
 
@@ -42,7 +43,7 @@ public class ConvertDocsTest {
       String table = "KRNS_MAINT_DOC_T";
       int batchSize = 75;
       int max = 10000000;
-      List<RowId> ids = getRowIds(props, table, max, 50000);
+      List<RowId> ids = shuffle(getRowIds(props, table, max, 50000));
       int show = 1000;
       DataMetrics overall = new DataMetrics();
       DataMetrics current = new DataMetrics();
