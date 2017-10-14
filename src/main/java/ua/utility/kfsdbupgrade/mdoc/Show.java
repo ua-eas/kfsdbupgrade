09c70393e5acd4e358724d64639d63dc46b4f29f
jcaddel <jcaddel@email.arizona.edu>
2017-10-14 03:26:14 -0700

    perf

--- src/main/java/ua/utility/kfsdbupgrade/mdoc/Show.java
+++ src/main/java/ua/utility/kfsdbupgrade/mdoc/Show.java
@@ -19,25 +19,32 @@ public final class Show {
   private static final Logger LOGGER = getLogger(Show.class);
 
   public static void show(DatabaseMetric snapshot) {
-    List<Object> args = newArrayList();
-    args.addAll(getArgs(snapshot.getOverall(), snapshot.getOverallWallTimeMicros()));
-    args.add(getTime(snapshot.getOverallWallTimeMicros() / 1000));
-    args.addAll(getArgs(snapshot.getCurrent(), snapshot.getCurrentWallTimeMicros()));
-    args.add(getTime(snapshot.getCurrentWallTimeMicros() / 1000));
-    info(LOGGER, "r%s/%s/%s|c%s/%s/%s|w%s/%s/%s|%s-r%s/%s/%s|c%s/%s/%s|w%s/%s/%s|%s", args.toArray());
+    if (snapshot.getOverall().getSelect().getCount() > 0) {
+      List<Object> args = newArrayList();
+      args.addAll(getArgs(snapshot.getOverall().getSelect(), snapshot.getOverallWallTimeMicros()));
+      args.addAll(getArgs(snapshot.getCurrent().getSelect(), snapshot.getCurrentWallTimeMicros()));
+      info(LOGGER, "read---->[%s/%s/%s|%s] [%s/%s/%s|%s] %s", args.toArray());
+    }
+    if (snapshot.getOverall().getConvert().getCount() > 0) {
+      List<Object> args = newArrayList();
+      args.addAll(getArgs(snapshot.getOverall().getConvert(), snapshot.getOverallWallTimeMicros()));
+      args.addAll(getArgs(snapshot.getCurrent().getConvert(), snapshot.getCurrentWallTimeMicros()));
+      info(LOGGER, "convert->[%s/%s/%s|%s] [%s/%s/%s|%s] %s", args.toArray());
+    }
+    if (snapshot.getOverall().getUpdate().getCount() > 0) {
+      List<Object> args = newArrayList();
+      args.addAll(getArgs(snapshot.getOverall().getUpdate(), snapshot.getOverallWallTimeMicros()));
+      args.addAll(getArgs(snapshot.getCurrent().getUpdate(), snapshot.getCurrentWallTimeMicros()));
+      info(LOGGER, "write--->[%s/%s/%s|%s] [%s/%s/%s|%s] %s", args.toArray());
+    }
   }
 
-  public static ImmutableList<Object> getArgs(MDocMetric metric, long micros) {
-    String rcount = getCount(checkedCast(metric.getSelect().getCount()));
-    String read = getIops(metric.getSelect(), micros);
-    String rtime = getTime(metric.getSelect().getMicroseconds() / 1000);
-    String ccount = getCount(checkedCast(metric.getConvert().getCount()));
-    String convert = getIops(metric.getConvert(), micros);
-    String ctime = getTime(metric.getConvert().getMicroseconds() / 1000);
-    String wcount = getCount(checkedCast(metric.getUpdate().getCount()));
-    String write = getIops(metric.getUpdate(), micros);
-    String wtime = getTime(metric.getUpdate().getMicroseconds() / 1000);
-    return ImmutableList.<Object>of(rcount, read, rtime, ccount, convert, ctime, wcount, write, wtime);
+  private static ImmutableList<Object> getArgs(DataMetric dm, long wallTimeMicros) {
+    String count = getCount(checkedCast(dm.getCount()));
+    String iops = getIops(dm, wallTimeMicros);
+    String time = getTime(dm.getMicroseconds() / 1000);
+    String wtime = getTime(wallTimeMicros / 1000);
+    return ImmutableList.<Object>of(count, iops, time, wtime);
   }
 
   private static String getIops(DataMetric metric, long microseconds) {
