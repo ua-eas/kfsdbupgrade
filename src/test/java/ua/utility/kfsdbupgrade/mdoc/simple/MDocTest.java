a4170841379e0b1ac2dbf8b3e3bad9521e0a2aae
jcaddel <jcaddel@email.arizona.edu>
2017-10-15 10:36:13 -0700

    perf

--- src/test/java/ua/utility/kfsdbupgrade/mdoc/simple/MDocTest.java
+++ src/test/java/ua/utility/kfsdbupgrade/mdoc/simple/MDocTest.java
@@ -66,7 +66,7 @@ public class MDocTest {
         String w = getThroughputInSeconds(write.getMetric().getMillis(), write.getMetric().getCount(), "").trim();
         String now = getThroughputInSeconds(current.elapsed(MILLISECONDS), chunk.size(), "").trim();
         String tp = getThroughputInSeconds(overall.elapsed(MILLISECONDS), count, "").trim();
-        info(LOGGER, "%s %s docs/sec %s latest[%s docs/sec r%s c%s w%s %s]", getCount(count), tp, getTime(overall), now, r, c, w, getTime(current));
+        info(LOGGER, "[%s %s docs/sec %s] now[%s docs/sec r%s c%s w%s %s]", getCount(count), tp, getTime(overall), now, r, c, w, getTime(current));
       }
     } catch (Throwable e) {
       e.printStackTrace();
