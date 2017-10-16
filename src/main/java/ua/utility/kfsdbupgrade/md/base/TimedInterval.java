265c3270bae8b80759845895224883c6438b12c0
jcaddel <jcaddel@email.arizona.edu>
2017-10-15 20:28:50 -0700

    perf

--- src/main/java/ua/utility/kfsdbupgrade/md/base/TimedInterval.java
+++ src/main/java/ua/utility/kfsdbupgrade/md/base/TimedInterval.java
@@ -1,4 +1,4 @@
-package ua.utility.kfsdbupgrade.mdoc;
+package ua.utility.kfsdbupgrade.md.base;
 
 import static com.google.common.base.MoreObjects.toStringHelper;
 import static com.google.common.base.Preconditions.checkArgument;
