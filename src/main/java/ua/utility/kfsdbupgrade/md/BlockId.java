7af7a9d605f254f44ee8fb27da2dac3fa8bac060
jcaddel <jcaddel@email.arizona.edu>
2017-10-16 07:13:42 -0700

    perf

--- src/main/java/ua/utility/kfsdbupgrade/md/BlockId.java
+++ src/main/java/ua/utility/kfsdbupgrade/md/BlockId.java
@@ -42,7 +42,7 @@ public final class BlockId implements Comparable<BlockId> {
       return false;
     } else {
       BlockId other = (BlockId) object;
-      return (hash == other.hash) && (fileNumber == other.fileNumber) && (blockNumber == other.blockNumber);
+      return (hash == other.hash) && compareTo(other) == 0;
     }
   }
 
