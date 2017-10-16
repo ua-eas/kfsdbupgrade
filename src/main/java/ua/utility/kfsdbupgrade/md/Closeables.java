111bb311ad14a8f3506f99e4d0355740b6b0a9ef
jcaddel <jcaddel@email.arizona.edu>
2017-10-15 20:18:21 -0700

    perf

--- src/main/java/ua/utility/kfsdbupgrade/md/Closeables.java
+++ src/main/java/ua/utility/kfsdbupgrade/md/Closeables.java
@@ -1,6 +1,10 @@
 package ua.utility.kfsdbupgrade.md;
 
+import org.apache.log4j.Logger;
+
 public final class Closeables {
+
+  private static final Logger LOGGER = Logger.getLogger(Closeables.class);
 
   public static void closeQuietly(AutoCloseable closeable) {
     if (closeable != null) {
@@ -8,6 +12,7 @@ public final class Closeables {
         closeable.close();
       } catch (Exception e) {
         e.printStackTrace();
+        LOGGER.error("unexpected closing error", e);
       }
     }
   }
