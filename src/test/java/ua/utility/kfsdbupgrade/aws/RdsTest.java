3c3605aebad8f7fbf66607fefb905ae03cf3b2f3
jcaddel <jcaddel@email.arizona.edu>
2017-11-11 10:20:43 -0700

    UAF-6014 stability

--- src/test/java/ua/utility/kfsdbupgrade/aws/RdsTest.java
+++ src/test/java/ua/utility/kfsdbupgrade/aws/RdsTest.java
@@ -1,15 +1,10 @@
 package ua.utility.kfsdbupgrade.aws;
 
-import static org.apache.log4j.Logger.getLogger;
-
-import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import com.amazonaws.services.rds.AmazonRDS;
 
 public final class RdsTest {
-
-  private static final Logger LOGGER = getLogger(RdsTest.class);
 
   @Test
   public void test() {
