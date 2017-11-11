f06bbd189e6f42060ff1808900f7d5199867c42f
jcaddel <jcaddel@email.arizona.edu>
2017-11-11 10:50:41 -0700

    UAF-6014 stability

--- src/test/java/ua/utility/kfsdbupgrade/aws/LatestSnapshotProvider.java
+++ src/test/java/ua/utility/kfsdbupgrade/aws/LatestSnapshotProvider.java
@@ -20,7 +20,6 @@ import org.apache.log4j.Logger;
 import com.amazonaws.services.rds.AmazonRDS;
 import com.amazonaws.services.rds.model.DBSnapshot;
 import com.amazonaws.services.rds.model.DescribeDBSnapshotsRequest;
-import com.google.common.base.Function;
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 
@@ -37,44 +36,20 @@ public final class LatestSnapshotProvider implements Provider<String> {
   private final AmazonRDS rds;
   private final String instanceId;
   private final boolean automatedOnly;
+  private final String automatedPrefix = "rds:";
 
   public String get() {
     DescribeDBSnapshotsRequest request = new DescribeDBSnapshotsRequest();
     request.setDBInstanceIdentifier(instanceId);
-    Predicate<DBSnapshot> predicate = (automatedOnly) ? AutomatedSnapshotPredicate.INSTANCE : Predicates.<DBSnapshot>alwaysTrue();
-    String log = (automatedOnly) ? format("instance:%s %s", instanceId, predicate) : "instance:" + instanceId;
+    Predicate<DBSnapshot> predicate = (automatedOnly) ? (ss) -> ss.getDBSnapshotIdentifier().startsWith(automatedPrefix) : Predicates.<DBSnapshot>alwaysTrue();
+    String log = (automatedOnly) ? format("instance:%s starts with:%s", instanceId, automatedPrefix) : "instance:" + instanceId;
     List<DBSnapshot> snapshots = rds.describeDBSnapshots(request).getDBSnapshots();
-    List<DBSnapshot> filtered = reverse(sort(snapshots, predicate, SnapshotCreationTime.INSTANCE));
+    List<DBSnapshot> filtered = reverse(sort(snapshots, predicate, (ss) -> ss.getSnapshotCreateTime().getTime()));
     checkState(filtered.size() > 0, "no snapshots found matching [%s]", log);
     DBSnapshot snapshot = filtered.iterator().next();
     info(LOGGER, "located %s snapshots matching [%s]", getCount(filtered.size()), log);
     info(LOGGER, "snapshot [%s] created on %s is the most recent", snapshot.getDBSnapshotIdentifier(), date(snapshot.getSnapshotCreateTime().getTime()));
     return snapshot.getDBSnapshotIdentifier();
-  }
-
-  private enum AutomatedSnapshotPredicate implements Predicate<DBSnapshot> {
-    INSTANCE;
-
-    private final String prefix = "rds:";
-
-    public boolean apply(DBSnapshot input) {
-      return input.getDBSnapshotIdentifier().startsWith(prefix);
-    }
-
-    @Override
-    public String toString() {
-      return "StartsWith(" + prefix + ")";
-    }
-
-  }
-
-  private enum SnapshotCreationTime implements Function<DBSnapshot, Long> {
-    INSTANCE;
-
-    public Long apply(DBSnapshot input) {
-      return input.getSnapshotCreateTime().getTime();
-    }
-
   }
 
 }
