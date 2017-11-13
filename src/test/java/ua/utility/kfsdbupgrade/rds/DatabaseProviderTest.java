33417c3fadef75626aab8f00ded6b064d3ac793f
jcaddel <jcaddel@email.arizona.edu>
2017-11-12 17:12:22 -0700

    UAF-6014 stability

--- src/test/java/ua/utility/kfsdbupgrade/rds/DatabaseProviderTest.java
+++ src/test/java/ua/utility/kfsdbupgrade/rds/DatabaseProviderTest.java
@@ -1,13 +1,21 @@
 package ua.utility.kfsdbupgrade.rds;
 
+import java.util.Properties;
+
 import org.junit.Test;
+
+import ua.utility.kfsdbupgrade.md.PropertiesProvider;
 
 public final class DatabaseProviderTest {
 
   @Test
   public void test() {
     try {
-      ProvideDatabaseRequest request = new ProvideDatabaseRequest("us-west-2", "kfs3imp", "kfs36014");
+      Properties properties = new PropertiesProvider().get();
+      String region = properties.getProperty("aws.region", "us-west-2");
+      String snapshotDatabase = properties.getProperty("rds.snapshot.name");
+      String instanceId = properties.getProperty("rds.name");
+      ProvideDatabaseRequest request = new ProvideDatabaseRequest(region, snapshotDatabase, instanceId);
       new DatabaseProvider(request).get();
     } catch (Throwable e) {
       e.printStackTrace();
