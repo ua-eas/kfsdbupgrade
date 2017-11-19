267b91264a36b1207979c80aaf0f2f395c32ad31
jcaddel <jcaddel@email.arizona.edu>
2017-11-19 16:55:01 -0700

    UAF-6014 stability

--- src/main/java/ua/utility/kfsdbupgrade/rds/Rds.java
+++ src/main/java/ua/utility/kfsdbupgrade/rds/Rds.java
@@ -24,7 +24,7 @@ public final class Rds {
     checkArgument(isAllLowerCase(name, true), "[%s] must be all lower case", name);
     checkArgument(!name.endsWith("-"), "[%s] name cannot end with a hyphen", name);
     checkArgument(!name.contains("--"), "[%s] name cannot contain consecutive hyphens", name);
-    checkArgument(name.length() < 63, "[%s] %s characters is too long, max is 63", name, name.length());
+    checkArgument(name.length() < 63, "[%s] %s characters is too long, max is 63", name, name.length() + 1);
     checkArgument(name.length() > 0, "[%s] name cannot be blank", name);
     checkArgument(isLowerCase(name.charAt(0), true), "[%s] name must start with a letter", name);
     checkArgument(isNormalized(name, true), "[%s] name must only contain letters, digits, and hyphens", name);
@@ -33,7 +33,7 @@ public final class Rds {
 
   public static String checkedSid(String sid) {
     checkArgument(isAllUpperCase(sid, false), "[%s] must be all upper case", sid);
-    checkArgument(sid.length() < 8, "[%s] %s characters is too long, max is 8", sid, sid.length());
+    checkArgument(sid.length() < 8, "[%s] %s characters is too long, max is 8", sid, sid.length() + 1);
     checkArgument(sid.length() > 0, "[%s] sid cannot be blank", sid);
     checkArgument(isNormalized(sid, false), "[%s] sid must only contain letters and digits", sid);
     return sid;
