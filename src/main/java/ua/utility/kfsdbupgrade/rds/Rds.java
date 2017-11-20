f7c3f497398cfda02836cd75b16823307d521a60
jcaddel <jcaddel@email.arizona.edu>
2017-11-19 19:12:33 -0700

    UAF-6014 stability

--- src/main/java/ua/utility/kfsdbupgrade/rds/Rds.java
+++ src/main/java/ua/utility/kfsdbupgrade/rds/Rds.java
@@ -24,16 +24,24 @@ public final class Rds {
     checkArgument(isAllLowerCase(name, true), "[%s] must be all lower case", name);
     checkArgument(!name.endsWith("-"), "[%s] name cannot end with a hyphen", name);
     checkArgument(!name.contains("--"), "[%s] name cannot contain consecutive hyphens", name);
-    checkArgument(name.length() < 63, "[%s] %s characters is too long, max is 63", name, name.length() + 1);
+    checkArgument(name.length() <= 63, "[%s] %s characters is too long, max is 63", name, name.length());
     checkArgument(name.length() > 0, "[%s] name cannot be blank", name);
     checkArgument(isLowerCase(name.charAt(0), true), "[%s] name must start with a letter", name);
     checkArgument(isNormalized(name, true), "[%s] name must only contain letters, digits, and hyphens", name);
     return name;
   }
 
+  /**
+   * https://asktom.oracle.com/pls/apex/f?p=100:11:::NO:RP:P11_QUESTION_ID:9535901800346738935 From the 12.2 documentation:
+   * 
+   * It is common practice to set the SID to be equal to the database name. The maximum number of characters for the database name is eight. For more information, see the
+   * discussion of the DB_NAME initialization parameter in Oracle Database Reference.
+   * 
+   * So even if you find a platform that does more than 8, I would never go more than that.
+   */
   public static String checkedSid(String sid) {
     checkArgument(isAllUpperCase(sid, false), "[%s] must be all upper case", sid);
-    checkArgument(sid.length() < 8, "[%s] %s characters is too long, max is 8", sid, sid.length() + 1);
+    checkArgument(sid.length() <= 8, "[%s] %s characters is too long, max is 8", sid, sid.length());
     checkArgument(sid.length() > 0, "[%s] sid cannot be blank", sid);
     checkArgument(isNormalized(sid, false), "[%s] sid must only contain letters and digits", sid);
     return sid;
