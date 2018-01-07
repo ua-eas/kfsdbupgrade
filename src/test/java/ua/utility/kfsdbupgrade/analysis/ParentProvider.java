75d45094e01a5d39ba94967cf683e05ab4c96f1d
jcaddel <jcaddel@email.arizona.edu>
2018-01-07 09:20:36 -0700

    UAF-6424

--- src/test/java/ua/utility/kfsdbupgrade/analysis/ParentProvider.java
+++ src/test/java/ua/utility/kfsdbupgrade/analysis/ParentProvider.java
@@ -4,8 +4,12 @@ import static com.google.common.base.Preconditions.checkArgument;
 import static java.lang.Integer.parseInt;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import static org.apache.commons.lang3.StringUtils.substringBetween;
+import static org.apache.commons.lang3.StringUtils.substringsBetween;
+import static ua.utility.kfsdbupgrade.md.base.Exceptions.illegalState;
+import static ua.utility.kfsdbupgrade.md.base.Lists.newList;
 
 import java.util.Iterator;
+import java.util.List;
 
 import javax.inject.Provider;
 
@@ -23,8 +27,13 @@ public final class ParentProvider implements Provider<BuildKey> {
   private final String buildXml;
 
   public BuildKey get() {
-    String causeAction = substringBetween(buildXml, "<hudson.model.CauseAction>", "</hudson.model.CauseAction>");
-    String upstreamProject = Xml.UnescapeFunction.INSTANCE.apply(substringBetween(causeAction, "<upstreamProject>", "</upstreamProject>"));
+    List<String> causeActions = newList(substringsBetween(buildXml, "<hudson.model.CauseAction>", "</hudson.model.CauseAction>"));
+    String causeAction = getCauseAction(causeActions);
+    String upstreamFragment = substringBetween(causeAction, "<upstreamProject>", "</upstreamProject>");
+    if (upstreamFragment == null) {
+      System.out.println("\n" + buildXml + "\n");
+    }
+    String upstreamProject = Xml.UnescapeFunction.INSTANCE.apply(upstreamFragment);
     String upstreamBuild = substringBetween(causeAction, "<upstreamBuild>", "</upstreamBuild>");
     if (upstreamProject.contains("/")) {
       Iterator<String> itr = Splitter.on('/').split(upstreamProject).iterator();
@@ -34,6 +43,15 @@ public final class ParentProvider implements Provider<BuildKey> {
     } else {
       return new BuildKey(upstreamProject, parseInt(upstreamBuild));
     }
+  }
+
+  private String getCauseAction(Iterable<String> causeActions) {
+    for (String causeAction : causeActions) {
+      if (causeAction.contains("<upstreamProject>")) {
+        return causeAction;
+      }
+    }
+    throw illegalState("could not locate upstream project");
   }
 
 }
